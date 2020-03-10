beb62c325926539438c1e3ba1c856e3c644ba355
suaadBatis <we_oo@hotmail.com>
2020-03-10 09:21:08 +0300

    fially

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,29 +2,33 @@
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import java.io.IOException;
 
 
-public class FirstClass {
+class FirstClass {
+  public class ScrolDwon extends base {
+  /*  @BeforeTest
+      public void killAllNodes() throws IOException, InterruptedException
+      {
+          //taskkill /F /IM node.exe
+          Runtime.getRuntime ().exec("/usr/bin/killall -KILL node");
+          Thread.sleep(3000);
+
+      }*/
 
 
-public class ScrolDwon extends base {
     @Test
-    public void start () throws IOException {
+    public void start () throws IOException, InterruptedException {
+        service = startServer();
         IOSDriver <IOSElement> driver = Capabilities ("Application");
         ScrolDownPageObject PageObject = new ScrolDownPageObject (driver);
         PageObject.getScrol1().click();
         Assert.assertTrue(PageObject.Scrol2.isDisplayed());
         utilities u= new utilities (driver);
         u.scrollToText (10,500,100,80,1000);
-    }
-    @AfterClass
-    public void teardown () {
-        if (driver != null) {
-            driver.quit ();
-        }
+        service.stop();
+
     }
  }
 }
